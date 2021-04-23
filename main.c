a4825811bced74a4f50164ed3cd1686f39a137b1
Shriya-265054 <shriya.naik0309@gmail.com>
2021-04-23 15:52:14 +0530

    upd

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <avr/io.h>
+#include<util/delay.h>
+int main(void)
+{
+    DDRD |= (1<<PD0); //set PB0=1 FOR LED
+    DDRC &= ~(1<<PC0); //set PC0=0, CLEAR BIT, "BUTTON"
+    DDRB &= ~(1<<PB0); // set PB0=0, "HEATER"
+    // Insert code
+    PORTC |= (1<<PC0); // SET BIT
+    PORTB |= (1<<PB0); //set bit
+    while(1)
+    {
+        if ((PINC &(1<<PC0)) && ((PINB & (1<<PB0))))
+        {
+            PORTD &= ~(1<<PD0);
+        }
+        else if ((PINC &(1<<PC0)) && (!(PINB &(1<<PB0))))
+        {
+            PORTD &= ~(1<<PD0);
+        }
+        else if ((!(PINC &(1<<PC0))) && (PINB &(1<<PB0)))
+        {
+            PORTD &= ~(1<<PD0);
+        }
+        else if ((!(PINC &(1<<PC0))) && (!(PINB &(1<<PB0))))
+        {
+            PORTD |= (1<<PD0);
+        }
+
+    }
+    return 0;
+}
