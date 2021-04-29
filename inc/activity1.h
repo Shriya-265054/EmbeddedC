175ebd13bacb357230cb3b201f76dd1cef9b3979
Shriya-265054 <shriya.naik0309@gmail.com>
2021-04-30 00:29:50 +0530

    activity1

--- inc/activity1.h
+++ inc/activity1.h
@@ -12,10 +12,7 @@
 #define __ACTIVITY1_H__
 #include<avr/io.h>
 
-#define BUTTON_ON !(PIND &(1<<PD0))
-#define HEATER_ON !(PIND &(1<<PD1))
-#define LED_OFF  PORTB &= ~(1<<PB0)
-#define LED_ON   PORTB |= (1<<PB0)
+
 /**
  * @brief Changing the state of LED according to Button sensor and Heater sensor
  * 
